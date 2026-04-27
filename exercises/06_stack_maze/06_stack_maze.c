3e46d5baefade2dc74fa6768d2a6ded79afc4e28
First-da <2082258001@qq.com>
2026-04-27 22:46:45 +0800

    练习8

--- exercises/06_stack_maze/06_stack_maze.c
+++ exercises/06_stack_maze/06_stack_maze.c
@@ -1,90 +1,25 @@
-#include <stdio.h>
-
-#define MAX_ROW 5
-#define MAX_COL 5
-
-struct point { int row, col; } stack[512];
-int top = 0;
-
-void push(struct point p)
-{
-	stack[top++] = p;
-}
-
-struct point pop(void)
-{
-	return stack[--top];
-}
-
-int is_empty(void)
-{
-	return top == 0;
-}
-
-int maze[MAX_ROW][MAX_COL] = {
-	0, 1, 0, 0, 0,
-	0, 1, 0, 1, 0,
-	0, 0, 0, 0, 0,
-	0, 1, 1, 1, 0,
-	0, 0, 0, 1, 0,
-};
-
-struct point predecessor[MAX_ROW][MAX_COL] = {
-	{{-1,-1}, {-1,-1}, {-1,-1}, {-1,-1}, {-1,-1}},
-	{{-1,-1}, {-1,-1}, {-1,-1}, {-1,-1}, {-1,-1}},
-	{{-1,-1}, {-1,-1}, {-1,-1}, {-1,-1}, {-1,-1}},
-	{{-1,-1}, {-1,-1}, {-1,-1}, {-1,-1}, {-1,-1}},
-	{{-1,-1}, {-1,-1}, {-1,-1}, {-1,-1}, {-1,-1}},
-};
-
-void visit(int row, int col, struct point pre)
-{
-	struct point visit_point = { row, col };
-	maze[row][col] = 2;
-	predecessor[row][col] = pre;
-	push(visit_point);
-}
-
-int main(void)
-{
-	struct point p = { 0, 0 };
-
-	maze[p.row][p.col] = 2;
-	push(p);	
-	
-	while (!is_empty()) {
-		p = pop();
-		if (p.row == MAX_ROW - 1  /* goal */
-		    && p.col == MAX_COL - 1)
-			break;
-		if (p.col+1 < MAX_COL     /* right */
-		    && maze[p.row][p.col+1] == 0)
-			visit(p.row, p.col+1, p);
-		if (p.row+1 < MAX_ROW     /* down */
-		    && maze[p.row+1][p.col] == 0)
-			visit(p.row+1, p.col, p);
-		if (p.col-1 >= 0          /* left */
-		    && maze[p.row][p.col-1] == 0)
-			visit(p.row, p.col-1, p);
-		if (p.row-1 >= 0          /* up */
-		    && maze[p.row-1][p.col] == 0)
-			visit(p.row-1, p.col, p);
-	}
-	if (p.row == MAX_ROW - 1 && p.col == MAX_COL - 1) {
-		printf("(%d, %d)\n", p.row, p.col);
-		while (predecessor[p.row][p.col].row != -1) {
-			p = predecessor[p.row][p.col];
-			printf("(%d, %d)\n", p.row, p.col);
-		}
-	} else
-		printf("No path!\n");
-
-	return 0;
-}
 // #include <stdio.h>
 
 // #define MAX_ROW 5
 // #define MAX_COL 5
+
+// struct point { int row, col; } stack[512];
+// int top = 0;
+
+// void push(struct point p)
+// {
+// 	stack[top++] = p;
+// }
+
+// struct point pop(void)
+// {
+// 	return stack[--top];
+// }
+
+// int is_empty(void)
+// {
+// 	return top == 0;
+// }
 
 // int maze[MAX_ROW][MAX_COL] = {
 // 	0, 1, 0, 0, 0,
@@ -94,9 +29,126 @@ int main(void)
 // 	0, 0, 0, 1, 0,
 // };
 
+// struct point predecessor[MAX_ROW][MAX_COL] = {
+// 	{{-1,-1}, {-1,-1}, {-1,-1}, {-1,-1}, {-1,-1}},
+// 	{{-1,-1}, {-1,-1}, {-1,-1}, {-1,-1}, {-1,-1}},
+// 	{{-1,-1}, {-1,-1}, {-1,-1}, {-1,-1}, {-1,-1}},
+// 	{{-1,-1}, {-1,-1}, {-1,-1}, {-1,-1}, {-1,-1}},
+// 	{{-1,-1}, {-1,-1}, {-1,-1}, {-1,-1}, {-1,-1}},
+// };
+
+// void visit(int row, int col, struct point pre)
+// {
+// 	struct point visit_point = { row, col };
+// 	maze[row][col] = 2;
+// 	predecessor[row][col] = pre;
+// 	push(visit_point);
+// }
+
 // int main(void)
 // {
-// 	// TODO: 在这里添加你的代码
+// 	struct point p = { 0, 0 };
+
+// 	maze[p.row][p.col] = 2;
+// 	push(p);	
+	
+// 	while (!is_empty()) {
+// 		p = pop();
+// 		if (p.row == MAX_ROW - 1  /* goal */
+// 		    && p.col == MAX_COL - 1)
+// 			break;
+// 		if (p.col+1 < MAX_COL     /* right */
+// 		    && maze[p.row][p.col+1] == 0)
+// 			visit(p.row, p.col+1, p);
+// 		if (p.row+1 < MAX_ROW     /* down */
+// 		    && maze[p.row+1][p.col] == 0)
+// 			visit(p.row+1, p.col, p);
+// 		if (p.col-1 >= 0          /* left */
+// 		    && maze[p.row][p.col-1] == 0)
+// 			visit(p.row, p.col-1, p);
+// 		if (p.row-1 >= 0          /* up */
+// 		    && maze[p.row-1][p.col] == 0)
+// 			visit(p.row-1, p.col, p);
+// 	}
+// 	if (p.row == MAX_ROW - 1 && p.col == MAX_COL - 1) {
+// 		printf("(%d, %d)\n", p.row, p.col);
+// 		while (predecessor[p.row][p.col].row != -1) {
+// 			p = predecessor[p.row][p.col];
+// 			printf("(%d, %d)\n", p.row, p.col);
+// 		}
+// 	} else
+// 		printf("No path!\n");
 
 // 	return 0;
 // }
+#include <stdio.h>
+
+#define MAX_ROW 5
+#define MAX_COL 5
+
+int maze[MAX_ROW][MAX_COL] = {
+	0, 1, 0, 0, 0,
+	0, 1, 0, 1, 0,
+	0, 0, 0, 0, 0,
+	0, 1, 1, 1, 0,
+	0, 0, 0, 1, 0,
+};
+
+typedef struct {
+	int pre[2];
+} pos;
+
+pos path[MAX_ROW * MAX_COL];
+int ptr;
+
+int is_empty()
+{
+	return ptr;
+}
+
+pos top()
+{
+	return path[ptr];
+}
+
+pos pop()
+{
+	return path[--ptr];
+}
+
+pos push(pos element)
+{
+	return path[ptr++] = element;
+}
+
+int main(void)
+{
+	pos current_pos;
+	current_pos.pre[0] = 0;
+	current_pos.pre[1] = 0;
+	while ((current_pos.pre[0] != 4) && (current_pos.pre[1] != 4))
+	{
+		//down 
+		while ((current_pos.pre[1] <= 4) && (!maze[current_pos.pre[0]][current_pos.pre[1]+1]))
+		{
+			push(current_pos);
+			current_pos.pre[1]++;
+		}
+
+		//right
+		// if ((current_pos.pre[0] + 1 <= 4) && (!maze[current_pos.pre[0] + 1 ][current_pos.pre[1]]))
+		// {
+		// 	push(current_pos);
+		// 	current_pos.pre[0]++;
+		// }
+		// else if (maze[current_pos.pre[0] + 1 ][current_pos.pre[1]])
+		// {
+
+		// }
+		// else if((current_pos.pre[0] + 1 > 4)||)
+		// else if 
+
+	}
+
+	return 0;
+}
--- exercises/08_circular_queue/08_circular_queue.c
+++ exercises/08_circular_queue/08_circular_queue.c
@@ -20,9 +20,35 @@ int main() {
     int report_interval=5;
 
     // TODO: 在这里添加你的代码
-    // I AM NOT DONE
-    
-    printf("最后剩下的人是: %d\n", q.data[q.head].id);
+    for (int i = 0;i < MAX_PEOPLE;i++)
+    {
+        q.data[i].id = i + 1;
+        q.count = total_people;
+    }
+
+    int idx = 0;
+    while(q.count != 0)
+    {
+        idx = (q.count == total_people) ? idx + q.count - 1 : idx + q.count;
+        if (idx > MAX_PEOPLE - 1)
+            idx -= MAX_PEOPLE ; 
+        if (q.data[idx].id) 
+            {
+                if (q.count == 1)
+                {
+                    printf("淘汰: %d\n", q.data[idx].id);
+                    q.count --;
+                    q.data[idx].id = 0;
+                }
+                else
+                {
+                    printf("最后剩下的人是: %d\n", q.data[idx].id);
+                    q.count --;
+                    q.data[idx].id = 0;
+                }
+                    
+            }
+    }
 
     return 0;
 }
