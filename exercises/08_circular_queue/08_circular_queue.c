40c245f9a1abd336f757203ad75cccff0926eabf
First-da <2082258001@qq.com>
2026-04-27 23:31:08 +0800

    练习08 success

--- exercises/08_circular_queue/08_circular_queue.c
+++ exercises/08_circular_queue/08_circular_queue.c
@@ -1,66 +1,22 @@
 #include <stdio.h>
-#include <stdbool.h>
-
-#define MAX_PEOPLE 50
-
-typedef struct {
-    int id;
-} People;
-
-typedef struct {
-    People data[MAX_PEOPLE];
-    int head;
-    int tail;
-    int count;
-} Queue;
 
 int main() {
-    Queue q;
-    int total_people=50;
-    int report_interval=5;
-
-    // TODO: 在这里添加你的代码
-    for (int i = 0;i < MAX_PEOPLE;i++)
-    {
-        q.data[i].id = i + 1;
-        q.count = total_people;
-    }
-
-    int idx = 0;
-    int step = 4;
-
-
-    while(q.count != 0)
-    {
-        if (q.data[idx].id == 0) 
-            {
-                idx ++;
-                continue;
-            }
-        idx ++;    
-        if (idx >= MAX_PEOPLE)
-            idx -=MAX_PEOPLE;
-        step --;
+    int people[50];
+    for (int i = 0; i < 50; i++) people[i] = i + 1;
+    
+    int current = 0;
+    int remaining = 50;
+    
+    while (remaining > 1) {
+        current = (current + 4) % remaining;
+        printf("淘汰: %d\n", people[current]);
         
-        if (step == 0)
-        {
-            if (q.count != 1)
-                {
-                    printf("淘汰: %d\n", q.data[idx].id);
-                    q.count --;
-                    q.data[idx].id = 0;
-                }
-            else
-                {
-                    printf("最后剩下的人是: %d\n", q.data[idx].id);
-                    q.count --;
-                    q.data[idx].id = 0;
-                }
-            
-            step = report_interval;
+        for (int i = current; i < remaining - 1; i++) {
+            people[i] = people[i + 1];
         }
+        remaining--;
     }
-
-
+    
+    printf("最后剩下的人是: %d\n", people[0]);
     return 0;
 }
