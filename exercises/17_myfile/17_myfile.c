b8470ba78440d6b80417d338ced94fab8393a097
First-da <2082258001@qq.com>
2026-04-28 20:01:03 +0800

    练习17

--- exercises/17_myfile/17_myfile.c
+++ exercises/17_myfile/17_myfile.c
@@ -8,8 +8,36 @@
 void print_elf_type(uint16_t e_type) {
   const char *type_str;
   switch (e_type) {
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    // ET_NONE  → Unknown / No file type
+    // ET_REL   → Relocatable
+    // ET_EXEC  → Executable
+    // ET_DYN   → Shared Object/PIE
+    // ET_CORE  → Core file
+    case (ET_NONE) :
+      {
+        type_str = "Unknown";
+        break;
+      }
+    case (ET_REL):
+      {
+        type_str = "Relocatable";
+        break;
+      }
+    case (ET_EXEC):
+      {
+        type_str = "Executable";
+        break;
+      }
+    case (ET_DYN):
+      {
+        type_str = "Shared Object/PIE";
+        break;
+      }    
+    case (ET_CORE):
+      {
+        type_str = "Core file";
+        break;
+      }
   }
   printf("ELF Type: %s (0x%x)\n", type_str, e_type);
 }
@@ -22,9 +50,41 @@ int main(int argc, char *argv[]) {
 
   int fd;
   Elf64_Ehdr ehdr;
+  fd = open(filepath[0], O_RDONLY);
+  if (fd < 0)
+    {
+      perror("open");
+      exit(1);
+    }  
 
-  // TODO: 在这里添加你的代码
-  // I AM NOT DONE
-  
+  int read_num;
+  read_num = read(fd, &ehdr, sizeof(ehdr));
+  if (read_num < 0)
+  {
+      perror("read");
+      exit(1);
+  }
+
+  print_elf_type(ehdr.e_type);
+  close(fd);
+
+
+
+  fd = open(filepath[1], O_RDONLY);
+  if (fd < 0)
+    {
+      perror("open");
+      exit(1);
+    }  
+
+  read_num = read(fd, &ehdr, sizeof(ehdr));
+  if (read_num < 0)
+  {
+      perror("read");
+      exit(1);
+  }
+
+  print_elf_type(ehdr.e_type);
+  close(fd);
   return 0;
 }
