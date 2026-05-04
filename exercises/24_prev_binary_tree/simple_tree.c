40999ad7f6ee14d616fa7ea2d6d639a749d6e1dd
First-da <2082258001@qq.com>
2026-05-04 19:48:57 +0800

    练习24

--- exercises/24_prev_binary_tree/simple_tree.c
+++ exercises/24_prev_binary_tree/simple_tree.c
@@ -11,13 +11,35 @@ Queue* create_queue() {
 }
 
 void enqueue(Queue *q, TreeNode *tree_node) {
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    QueueNode *new_node = (QueueNode*)malloc(sizeof(QueueNode));
+    new_node->tree_node = tree_node;
+    new_node->next = NULL;
+    
+    if (is_empty(q)) {
+        q->front = new_node;
+        q->rear = new_node;
+    } else {
+        q->rear->next = new_node;
+        q->rear = new_node;
+    }
 }
 
 TreeNode* dequeue(Queue *q) {
     // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+        if (is_empty(q)) {
+        return NULL;
+    }
+    
+    QueueNode *temp = q->front;
+    TreeNode *tree_node = temp->tree_node;
+    q->front = q->front->next;
+    
+    if (q->front == NULL) {
+        q->rear = NULL;
+    }
+    
+    free(temp);
+    return tree_node;
 }
 
 bool is_empty(Queue *q) {
@@ -33,17 +55,80 @@ void free_queue(Queue *q) {
 
 TreeNode* build_tree_by_level(int *level_order, int size) {
     // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+        if (size == 0 || level_order[0] == INT_MIN) {
+        return NULL;
+    }
+    
+    TreeNode *root = (TreeNode*)malloc(sizeof(TreeNode));
+    root->val = level_order[0];
+    root->left = NULL;
+    root->right = NULL;
+    
+    Queue *q = create_queue();
+    enqueue(q, root);
+    
+    int i = 1;
+    while (!is_empty(q) && i < size) {
+        TreeNode *parent = dequeue(q);
+        
+        // 左孩子
+        if (level_order[i] != INT_MIN) {
+            TreeNode *left = (TreeNode*)malloc(sizeof(TreeNode));
+            left->val = level_order[i];
+            left->left = NULL;
+            left->right = NULL;
+            parent->left = left;
+            enqueue(q, left);
+        }
+        i++;
+        
+        // 右孩子
+        if (i < size && level_order[i] != INT_MIN) {
+            TreeNode *right = (TreeNode*)malloc(sizeof(TreeNode));
+            right->val = level_order[i];
+            right->left = NULL;
+            right->right = NULL;
+            parent->right = right;
+            enqueue(q, right);
+        }
+        i++;
+    }
+    
+    free_queue(q);
+    return root;
 }
 
 void preorder_traversal(TreeNode *root) {
     // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    if (root == NULL)
+        return ;
+    printf("%d ", root->val);
+
+    preorder_traversal(root->left);
+    preorder_traversal(root->right);
 }
 
 void preorder_traversal_iterative(TreeNode *root) {
     // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    if (root == NULL) return;
+    
+    TreeNode *stack[1000];  // 简单用数组模拟栈
+    int top = -1;
+    
+    stack[++top] = root;
+    
+    while (top >= 0) {
+        TreeNode *node = stack[top--];
+        printf("%d ", node->val);
+        
+        // 先压右孩子，再压左孩子（这样左孩子先出栈）
+        if (node->right) {
+            stack[++top] = node->right;
+        }
+        if (node->left) {
+            stack[++top] = node->left;
+        }
+    }
 }
 
 void free_tree(TreeNode *root) {
