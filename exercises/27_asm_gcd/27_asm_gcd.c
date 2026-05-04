c48973385b2438f6342095dd28b3cdfe7cf730b6
First-da <2082258001@qq.com>
2026-05-04 22:27:41 +0800

    练习27

--- exercises/27_asm_gcd/27_asm_gcd.c
+++ exercises/27_asm_gcd/27_asm_gcd.c
@@ -3,24 +3,25 @@
 unsigned int gcd_asm(unsigned int a, unsigned int b) {
     unsigned int result;
     
-    // TODO: 替换下方的TODO段
-    // I AM NOT DONE
     __asm__ volatile (
-        "mov %1, %%eax\n\t"     // 将a存入eax
-        "mov %2, %%ebx\n\t"     // 将b存入ebx
-        "jmp .L_check\n\t"      // 跳转到检查条件
+        "mov %1, %%eax\n\t"     // eax = a
+        "mov %2, %%ebx\n\t"     // ebx = b
+        "jmp .L_check\n\t"
         
         ".L_loop:\n\t"
-        "   TODO\n\t"
+        "   xor %%edx, %%edx\n\t"   // edx 清零，因为 div 用 edx:eax 作为被除数
+        "   divl %%ebx\n\t"         // eax = eax / ebx, edx = eax % ebx
+        "   mov %%ebx, %%eax\n\t"   // a = b
+        "   mov %%edx, %%ebx\n\t"   // b = 余数
         
         ".L_check:\n\t"
-        "   test TODO\n\t"      // 测试ebx是否为0
-        "   jne .L_loop\n\t"    // 如果不为0，继续循环
+        "   test %%ebx, %%ebx\n\t"  // 判断 b 是否为 0
+        "   jne .L_loop\n\t"
         
-        "mov %%eax, %0"         // 将结果（eax）存入输出变量
-        : "=r" (TODO)           // 输出操作数
-        : "r" (TODO), "r" (TODO)      // 输入操作数
-        : "TODO", "TODO", "TODO" // 被修改的寄存器
+        "mov %%eax, %0"
+        : "=r" (result)
+        : "r" (a), "r" (b)
+        : "eax", "ebx", "edx", "cc"
     );
     
     return result;
