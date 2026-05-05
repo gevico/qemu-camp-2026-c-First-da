d422a0e55482623f1f3e52a7175c45742c7b8b89
First-da <2082258001@qq.com>
2026-05-05 14:18:38 +0800

    练习30

--- exercises/30_debug_print/30_debug_print.c
+++ exercises/30_debug_print/30_debug_print.c
@@ -2,47 +2,47 @@
 #include <stdlib.h>
 #include <execinfo.h>
 
-#ifdef TODO
+#if DEBUG_LEVEL==1
 
-#define DEBUG_PRINT(fmt, ...) 
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+#define DEBUG_PRINT(fmt, ...)                               \
+    do                                                      \
+    {                                                       \
+        printf("DEBUG: func=%s, line=%d\n",__func__,__LINE__);  \
 
+    }while(0)       
+
+#elif DEBUG_LEVEL==2
+
+#define DEBUG_PRINT(fmt, ...)                               \
+    do                                                      \
+    {                                                       \
+        printf("DEBUG: func=%s, line=%d, ",__func__,__LINE__);\
+        printf(fmt, __VA_ARGS__);\
+        printf("\n");\
+    }while(0)      
+
+#elif DEBUG_LEVEL==3
+#define DEBUG_PRINT(fmt, ...)                           \
+    do                                                  \
+    {                                                   \
+        void *buffer[100];                              \
+        int n;                                          \
+        printf("DEBUG: func=%s, line=%d, ",__func__,__LINE__);\
+        printf(fmt, __VA_ARGS__);                       \
+        printf("\n");                                   \
+        n = backtrace(buffer,100);                      \
+        char **symbols;                            \
+        symbols = backtrace_symbols(buffer, n);         \
+        for(int i = 0; i < n; i++)                      \
+        {                                               \
+            printf("%s\n",symbols[i]);                  \
+        }                                               \  
+        free(symbols);                                  \
+    }while(0)
 #else
-
 #define DEBUG_PRINT(fmt, ...) do {} while (0)
 
 #endif
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//! MUST BE ENSURE THE DEBUG_PRINT("x=%d", x) AT THE 48 LINE
-
-// 测试代码
 void test() {
     int x = 42;
     DEBUG_PRINT("x=%d", x);
